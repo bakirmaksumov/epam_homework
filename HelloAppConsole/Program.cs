06de9f1b543dc5437366047eb0255132b00e78c5
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-07-12 11:12:01 +0500

    initial

---
+++ HelloAppConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте введите свое имя:");
+            string name = Console.ReadLine();     
+            Console.WriteLine($"Привет {name}");
+            Console.ReadLine();
+
+        }
+    }
+}
